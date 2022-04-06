1392373209cfa9745120a372e2732118b7c852ca
manjari23 <55528555+manjari23@users.noreply.github.com>
2022-04-06 17:30:50 +0530

    added menu bar

--- src/Header.js
+++ src/Header.js
@@ -8,19 +8,53 @@ function Header() {
   return (
     <>
       <header className='header'>
-        <div className="row">
-          <div className="col-1" id='Trendings_div'>Trendings</div>
-          <div className='col-6' id=''>Please find the latest news update here!</div>
-          <div className='col-4' id=''>  
-           <a href="#" class="fa fa-facebook" style={{ color: 'white',margin:"5px" }}></a>
-            <a href="#" class="fa fa-twitter" style={{ color: 'white',margin:"5px"  }}></a>
-            <a href="#" class="fa fa-google" style={{ color: 'white',margin:"5px"  }}></a>
-            <a href="#" class="fa fa-linkedin" style={{ color: 'white',margin:"5px"  }}></a>
-            </div>
-
-
+        <div className="row text-center">
+          <div className="col-1 text-right"></div>
+          <div className="col-1 text-right" id='Trendings_div'>Trendings</div>
+          <div className='col-8 text-center' id='header_col'>Please find the latest news update here!</div>
+          <div className='col-2' >
+            <a href="#" class="fa fa-facebook" ></a>
+            <a href="#" class="fa fa-twitter" ></a>
+            <a href="#" class="fa fa-google" ></a>
+            <a href="#" class="fa fa-linkedin" ></a>
+          </div>
         </div>
       </header>
+      <div className='row text-center'><h1 id="Grace_news_id">Grace News</h1></div>
+      <div className='row text-center'><p >Lorem ipsum dummy fames ac ante text</p></div>
+      <div className='row'>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid ">
+           
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">News & Trending</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Fashion & Lifestyle</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Business & Economy</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Healthy Life</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Contact us</a>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+        </nav>
+      </div>
     </>
   );
 }
