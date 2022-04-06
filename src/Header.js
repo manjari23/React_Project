0c02c8c4e1b47e52ae7845d3c56f806d638fe6b1
manjari23 <55528555+manjari23@users.noreply.github.com>
2022-04-06 19:42:49 +0530

    added menu bar

--- src/Header.js
+++ src/Header.js
@@ -13,47 +13,50 @@ function Header() {
           <div className="col-1 text-right" id='Trendings_div'>Trendings</div>
           <div className='col-8 text-center' id='header_col'>Please find the latest news update here!</div>
           <div className='col-2' >
-            <a href="#" class="fa fa-facebook" ></a>
-            <a href="#" class="fa fa-twitter" ></a>
-            <a href="#" class="fa fa-google" ></a>
-            <a href="#" class="fa fa-linkedin" ></a>
+            <a href="#" className="fa fa-facebook" ></a>
+            <a href="#" className="fa fa-twitter" ></a>
+            <a href="#" className="fa fa-google" ></a>
+            <a href="#" className="fa fa-linkedin" ></a>
           </div>
         </div>
       </header>
       <div className='row text-center'><h1 id="Grace_news_id">Grace News</h1></div>
       <div className='row text-center'><p >Lorem ipsum dummy fames ac ante text</p></div>
       <div className='row'>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container-fluid ">
-           
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid ">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
-                <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="#">Home</a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+                <li className="nav-item">
+                  <a className="nav-link" aria-current="page" href="#">Home</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">News & Trending</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">News & Trending</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Fashion & Lifestyle</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Fashion & Lifestyle</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Business & Economy</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Business & Economy</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Healthy Life</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Healthy Life</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Contact us</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Contact us</a>
                 </li>
               </ul>
 
             </div>
           </div>
         </nav>
+        {/* nav ends */}
+        {/* card starts */}
+        <div className='row'>
+          </div>
       </div>
     </>
   );
